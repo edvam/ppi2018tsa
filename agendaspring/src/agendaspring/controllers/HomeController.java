349713a29a3b539e5425e16c5b8544f2d22b5c26
20151074010018 <20151074010018@MC266441.ifrn.local>
2018-10-01 10:35:22 -0300

    Importando dependencias e adicionanado o método home.

--- agendaspring/src/agendaspring/controllers/HomeController.java
+++ agendaspring/src/agendaspring/controllers/HomeController.java
@@ -1,6 +1,15 @@
 package agendaspring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class HomeController {
+	
+	
+	@RequestMapping("/")
+	public void home() {
+		System.out.println("Chamou o meu Controller");
+	}
 
 }
